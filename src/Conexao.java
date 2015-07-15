7a2f5921c727774f6dc4d0e90dc499b6acdb7ea3
Juliana Rosendo Giacomo <ju.giacomo@gmail.com>
2015-07-15 12:04:01 -0300

    Concerto na criação da conexão com banco

--- src/Conexao.java
+++ src/Conexao.java
@@ -4,16 +4,39 @@ import java.sql.SQLException;
 
 
 public class Conexao {
-	private Connection conn;
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql:://127.0.0.1/";
+    private static final String BANCO = "gstok";
+    private static final String USUARIO = "gstok_user";
+    private static final String SENHA = "gs_p@$$*!";
+    private Connection conn;
 	
 	public Conexao(){
 		this.conn = null;
 	}
 	
-	public Connection getConexao() throws SQLException, ClassNotFoundException{
-		Class.forName("com.mysql.jdbc.Driver");
-		this.conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/gstok","gstok_user","gs_p@$$*!");
-		return this.conn;
-	}
+	public Connection getConexao() {
+               
+                try {
+                        Class.forName(DRIVER);
+                        this.conn = DriverManager.getConnection(HOST+BANCO, USUARIO, SENHA);
+                        return this.conn;
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+               
+                return null;
+               
+        }
+       
+        public void fecharConexao(){
+                if( this.conn != null ){
+                        try {
+                                this.conn.close();
+                        } catch (Exception e) {
+                                e.printStackTrace();
+                        }
+                }
+        }
 	
 }
