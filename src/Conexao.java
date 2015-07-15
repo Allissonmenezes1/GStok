0892cb324f6aaf44e016f921078f2a8cf9b7cb6a
Juliana Rosendo Giacomo <ju.giacomo@gmail.com>
2015-07-15 18:23:47 -0300

    correção na conexão

--- src/Conexao.java
+++ src/Conexao.java
@@ -1,42 +1,39 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-
 
 public class Conexao {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String HOST = "jdbc:mysql:://127.0.0.1/";
+    private static final String HOST = "jdbc:mysql://127.0.0.1/";
     private static final String BANCO = "gstok";
     private static final String USUARIO = "gstok_user";
     private static final String SENHA = "gs_p@$$*!";
     private Connection conn;
 	
-	public Conexao(){
-		this.conn = null;
-	}
+    public Conexao(){
+	this.conn = null;
+    }
 	
-	public Connection getConexao() {
+    public Connection getConexao() {
                
-                try {
-                        Class.forName(DRIVER);
-                        this.conn = DriverManager.getConnection(HOST+BANCO, USUARIO, SENHA);
-                        return this.conn;
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
-               
-                return null;
-               
+        try {
+            Class.forName(DRIVER);
+            this.conn = DriverManager.getConnection(HOST+BANCO, USUARIO, SENHA);
+            return this.conn;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+               
+        return null;
+               
+    }
        
-        public void fecharConexao(){
-                if( this.conn != null ){
-                        try {
-                                this.conn.close();
-                        } catch (Exception e) {
-                                e.printStackTrace();
-                        }
-                }
+    public void fecharConexao(){
+        if( this.conn != null ){
+            try {
+                this.conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-	
+    }
 }
