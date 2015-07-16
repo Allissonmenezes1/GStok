31c4324be215ba6a924eb8045b07c3d906692d65
A. Lopes <alyssonlopes@yahoo.com>
2015-07-16 16:04:58 -0300

    Adição de popup para cadastrar marca

---
+++ src/GUI/CadastrarMarca.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.JOptionPane;
+
+public class CadastrarMarca {
+    public static void main(String[] args) {
+        String marca = null;
+        marca = JOptionPane.showInputDialog("Informe o nome da marca");
+    }
+}
