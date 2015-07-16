ee011a48f3319c8bd9c46ae60a226788fc22919d
A. Lopes <alyssonlopes@yahoo.com>
2015-07-16 16:04:38 -0300

    Adição de popup para cadastrar categoria

---
+++ src/GUI/CadastrarCategoria.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.JOptionPane;
+
+public class CadastrarCategoria {
+    public static void main(String[] args) {
+        String categoria = null;
+        categoria = JOptionPane.showInputDialog("Informe nova categoria");
+    }
+}
