18e02a93af6c24fa63d5fab4120594bf73670fd5
Juliana Rosendo Giacomo <ju.giacomo@gmail.com>
2015-07-17 01:04:19 -0300

    Criação da classe ProdutoBO

---
+++ src/BO/ProdutoBO.java
@@ -0,0 +1,49 @@
+
+package BO;
+
+import DAO.ProdutoDAO;
+import PO.Produto;
+import java.util.ArrayList;
+
+public class ProdutoBO {
+    private ProdutoDAO p;
+    
+    public ProdutoBO(){
+        p = new ProdutoDAO();
+    }
+    
+    
+    public boolean cadastrarProduto(Produto produto, int cpfFuncionario){
+        
+        if(p.buscarProduto(produto.getCodBarras()) != null)
+            return false;
+        
+        return p.inserirProduto(produto, cpfFuncionario);
+        
+    }
+    
+    public boolean cadastrarMarca(String marca){
+        
+        ArrayList<String> m = p.buscarMarca();
+        
+        for(int i = 0; i < m.size(); i++){
+            if(m.get(i).equalsIgnoreCase(marca))
+                return false;
+        }
+        
+        return p.inserirMarca(marca);
+    }
+    
+    public boolean cadastrarCategoria(String categoria){
+        
+        ArrayList<String> c = p.buscarCategoria();
+        
+        for(int i = 0; i < c.size(); i++){
+            if(c.get(i).equalsIgnoreCase(categoria))
+                return false;
+        }
+        
+        return p.inserirCategoria(categoria);
+    }
+    
+}
