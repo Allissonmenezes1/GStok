d298a997927f12aa68b866af7befa6fafeb223c7
Juliana Rosendo Giacomo <ju.giacomo@gmail.com>
2015-07-17 02:12:49 -0300

    criação de um savepoint e rollback

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -2,6 +2,10 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexao {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
@@ -10,9 +14,11 @@ public class Conexao {
     private static final String USUARIO = "gstok_user";
     private static final String SENHA = "gs_p@$$*!";
     private Connection conn;
+    private Savepoint savepoint;
 	
     public Conexao(){
 	this.conn = null;
+        this.savepoint = null;
     }
 	
     public Connection getConexao() {
@@ -36,6 +42,22 @@ public class Conexao {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+    
+    public void savepoint(){
+        try {
+            this.savepoint = this.conn.setSavepoint();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void rollback(){
+        try {
+            this.conn.rollback(this.savepoint);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
