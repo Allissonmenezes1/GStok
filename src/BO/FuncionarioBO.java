3db0fdc13c6bb4271d27da4f93dee1deada91790
Juliana Rosendo Giacomo <ju.giacomo@gmail.com>
2015-07-17 00:09:56 -0300

    Criação da função login

---
+++ src/BO/FuncionarioBO.java
@@ -0,0 +1,25 @@
+
+package BO;
+import DAO.FuncionarioDAO;
+import PO.Funcionario;
+
+public class FuncionarioBO {
+    private FuncionarioDAO f;
+    
+    public FuncionarioBO(){
+        f = new FuncionarioDAO();
+    }
+    
+    public Funcionario login(int cpf, String senha){
+        
+        Funcionario func = f.getFuncionario(cpf);
+        
+        if(func == null)
+            return null;
+        if(func.getSenha().equals(senha))
+            return func;
+        else
+            return null;
+        
+    }
+}
