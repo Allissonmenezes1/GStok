f257b1d1459635bcc8e36176915af86c57dad1ba
Juliana Rosendo Giacomo <ju.giacomo@gmail.com>
2015-07-17 03:04:16 -0300

    Apangando import desnecessário

--- src/DAO/VendaDAO.java
+++ src/DAO/VendaDAO.java
@@ -4,9 +4,6 @@ package DAO;
 import PO.Produto;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import util.Conexao;
 
 public class VendaDAO {
